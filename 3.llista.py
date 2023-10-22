d3879cfd28bbf409f30381d57893723020259729
GERARDROCA <131850354+GERARDROCA@users.noreply.github.com>
2023-10-22 18:04:33 +0200

    Update 3.llista.py

--- 3.llista.py
+++ 3.llista.py
@@ -1,101 +1,98 @@
 import cv2
-import os
+import numpy as np
+from tensorflow.keras.models import load_model
 import tkinter as tk
 
-datapath = ' aquí va el dataset' ##############
-imagepaths = os.listdir(datapath)
-print('imagepath=', imagepaths)
+#Càrrega del model entrenat
+model = load_model('aquí va el model.h5')
 
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-face_recognizer.read('ModeloFaceFrontalData1.xml')
 
+#Carregar el classificador de cares de Haar
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+#Persones
+alumnes = ['persona1,persona2,persona3']  #PERSONES (en ordre)
+
+#Finestra
+finestra = tk.Tk()
+finestra.title("LLISTA D'ASSISTÈNCIA PER RECONEIXEMENT FACIAL")
+finestra.geometry('400x300')
+
+#Llista de presència amb StringVar(wideget que s'actualitza sol)
+ll_pres = []
+for class_label in alumnes:
+    ll_pres.append(tk.StringVar())
+    ll_pres[-1].set('Absent')
+
+#S'afeigexen els noms a la llista
+label_list = []
+for i, class_label in enumerate(alumnes):
+    label = tk.Label(finestra, text=class_label)
+    label.pack()
+    label_list.append(label)
+
+#Actualitzar l'assitència
+def act_assistencia(index, v):
+    ll_pres[index].set(v)
+
+# Actualitzar les etiquetes 
+def act_labels():
+    for i, label in enumerate(label_list):
+        label.config(text=f"{alumnes[i]}: {ll_pres[i].get()}") #Combina el nom amb l'assistència
+    finestra.after(1000, act_labels) #Actualitzar cada segon
+
+
+
+act_labels()
+
+#Iniciar video
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-faceclassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-# Llista d'estudiants
-estudiants = ['aquí va la llista dels estudiants, en el ordre que estan al dataset']
-
-# Diccionari per a l'estat de llista
-estat_llista = {estudiant: 'Desconegut' for estudiant in estudiants}
-
-def actualitzar_llista():
-    llista_presentats = []
-    llista_absents = []
-
-    for estudiant in estudiants:
-        if estat_llista[estudiant] == 'Present':
-            llista_presentats.append(estudiant)
-        elif estat_llista[estudiant] == 'Absent':
-            llista_absents.append(estudiant)
-
-    print("Estudiants presents:", llista_presentats)
-    print("Estudiants absents:", llista_absents)
-
-    # Actualitzar la llista a la finestra de l'aplicació
-    listbox.delete(0, tk.END)
-    for estudiant in estudiants:
-        listbox.insert(tk.END, f"{estudiant}: {estat_llista[estudiant]}")
-
-def capturar_frame():
+def process_frame():
+    #Capturar el fotograma actual
     ret, frame = cap.read()
-    if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        auxframe = gray.copy()
-        faces = faceclassif.detectMultiScale(gray, 1.3, 5)
 
-        for (x, y, w, h) in faces:
-            rostro = auxframe[y:y+h, x:x+w]
-            rostro = cv2.resize(rostro, (150, 105), interpolation=cv2.INTER_CUBIC)
-            result = face_recognizer.predict(rostro)
-            cv2.putText(frame, '{}'.format(result), (x, y-5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
+    if not ret:
+        return
 
-            if result[1] < 110:
-                nom_estudiant = imagepaths[result[0]].split('.')[0]
-                cv2.putText(frame, '{}'.format(nom_estudiant), (x, y-25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    #Convertir la imatge a escala de grisos
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                # Actualitzar l'estat de llista
-                if nom_estudiant in estat_llista:
-                    estat_llista[nom_estudiant] = 'Present'
-                    actualitzar_llista()  # Actualitzar la llista aquí
+    #Detectar cares a la imatge
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-            else:
-                cv2.putText(frame, 'Desconegut', (x, y-20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    #Processar cada cara detectada
+    for (x, y, w, h) in faces:
+        #Extraure la regió de la cara
+        cara = frame[y:y + h, x:x + w].copy()
 
-        cv2.imshow('frame', frame)
+        #Preprocessament de la imatge de la cara
+        cara = cv2.resize(cara, (224, 224))
+        cara = cara / 255.0  # Normalitzar els píxels
+        cara = np.expand_dims(cara, axis=0)
 
-        # Programar una nova captura de frame després de 1 mil·lisegon
-        window.after(1, capturar_frame)
-    else:
-        cap.release()
-        cv2.destroyAllWindows()
+        #Predir l'etiqueta de classe de la cara utilitzant el model
+        prediccions = model.predict(cara)
+        predicted_class_index = np.argmax(prediccions)
+        predicted_class_label = alumnes[predicted_class_index]
 
-def tancar_finestra():
-    cap.release()
-    cv2.destroyAllWindows()
-    window.destroy()
+        #Actualitzar assistència
+        ll_pres[predicted_class_index].set('Present')
 
-# Crear la finestra de l'aplicació
-window = tk.Tk()
-window.title("Aplicació de llista d'estudiants")
-window.geometry("400x300")
+    #Mostrar el fotograma amb les deteccions
+    cv2.imshow('Reconeixement Facial', frame)
 
-# Crear un Listbox per mostrar la llista d'estudiants
-listbox = tk.Listbox(window)
-listbox.pack()
+    #Sortir del bucle amb q
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        return
 
-# Botó per actualitzar la llista
-btn_actualitzar = tk.Button(window, text="Actualitzar llista", command=actualitzar_llista)
-btn_actualitzar.pack()
+    #Continuar processant els fotogrames
+    finestra.after(1, process_frame)
 
-# Botó per tancar la finestra
-btn_tancar = tk.Button(window, text="Tancar finestra", command=tancar_finestra)
-btn_tancar.pack()
+#Iniciar el processament dels fotogrames
+process_frame()
 
-# Capturar el primer frame
-capturar_frame()
+finestra.mainloop()
 
-# Iniciar el bucle principal de l'aplicació
-window.mainloop()
+cap.release()
+cv2.destroyAllWindows()
